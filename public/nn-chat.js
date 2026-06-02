f4330ef8f27447015d640e4815348727e07232eb
Muramoto Jun <koutya418@gmail.com>
2026-06-02 09:20:28 +0900

    投稿時刻の表示を変更

--- public/nn-chat.js
+++ public/nn-chat.js
@@ -1,4 +1,11 @@
 'use strict';
+
+// ツールチップの有効化
+const tooltipTriggerElements = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+tooltipTriggerElements.forEach((tooltipTriggerElement) => {
+  new bootstrap.Tooltip(tooltipTriggerElement);
+});
+
 // 一番下を表示
 window.addEventListener('load', () => {
   window.scrollTo(0, document.body.scrollHeight);
@@ -29,3 +36,4 @@ function isPressedSubmitKey(event) {
     return true;
   }
 }
+
